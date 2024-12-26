eea89981359571cd7f04fcdd68da6057a385a186
iBabs <ibrahimbabalola8@gmail.com>
2024-12-26 13:40:00 +0100

    Add favicon to layout and include hiaku.svg icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,9 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/hiaku.svg" sizes="any" />
+      </head>
       <body className="">
         <header className=" px-10 shadow-lg"><Navigation/></header>
         <main className="container mx-auto min-h-96 py-5 px-10">{children}</main>
